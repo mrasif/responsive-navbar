c5427b763db64d74522c74b8a6c5460527be5383
Asif Mohammad Mollah <admin@mrasif.in>
2020-02-02 22:15:59 +0530

    Updates

---
+++ precache-manifest.eda83fde1f3426496dcbdf8afd64b4a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00273cd138f5a530c3f03e5df7ce192d",
+    "url": "/responsive-navbar/index.html"
+  },
+  {
+    "revision": "c95e1314fc689efe04e6",
+    "url": "/responsive-navbar/static/css/main.2af83d60.chunk.css"
+  },
+  {
+    "revision": "66cb3295395898db2294",
+    "url": "/responsive-navbar/static/js/2.d71bc5c6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/responsive-navbar/static/js/2.d71bc5c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c95e1314fc689efe04e6",
+    "url": "/responsive-navbar/static/js/main.35923a68.chunk.js"
+  },
+  {
+    "revision": "a51a079e53bb5a71ca73",
+    "url": "/responsive-navbar/static/js/runtime-main.a8132b96.js"
+  }
+]);
